26a6bb73478b1a0769d98c05f31d2eb2ccd30f14
benji703 <benjadcool@gmail.com>
2022-11-15 10:37:11 +0100

    initial RShiny setup

---
+++ AlbumLength/app.R
@@ -0,0 +1,63 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(DT)
+library(readr)
+
+
+# Dataset
+Albums<-read_csv("./Album.csv")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  br(),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "y", label = "Y-axis:",
+        choices = c("Minutes", "Hours", "Sales", "Tracks"),
+        selected = "audience_score"
+      ),
+      
+      selectInput(
+        inputId = "x", label = "X-axis:",
+        choices = c("Year", "Minutes", "Genre"),
+        selected = "critics_score"
+      )
+    ),
+    
+    mainPanel(
+      plotOutput(outputId = "scatterplot", brush = "plot_brush"),
+      DT::dataTableOutput(outputId = "moviestable"),
+      br(),
+      plotOutput(outputId = "artistBar")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  output$scatterplot <- renderPlot({
+    ggplot(data = Albums, aes_string(x = input$x, y = input$y, color = "Genre")) +
+      geom_point()
+  })
+  
+  output$moviestable <- renderDataTable({
+    brushedPoints(Albums, brush = input$plot_brush) %>%
+      select(Year, Artist, Album, Minutes, Sales)
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
