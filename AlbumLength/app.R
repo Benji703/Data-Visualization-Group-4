7b9a07d58cc085d8b6e94cb0f7438e016390418e
benji703 <benjadcool@gmail.com>
2022-12-05 12:24:09 +0100

    Add barchart that shows top ten albums of the selected genre

--- AlbumLength/app.R
+++ AlbumLength/app.R
@@ -12,6 +12,7 @@ library(ggplot2)
 library(dplyr)
 library(DT)
 library(readr)
+library(tidyr)
 
 
 # Dataset
@@ -25,23 +26,32 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput(
         inputId = "y", label = "Y-axis:",
-        choices = c("Minutes", "Hours", "Sales", "Tracks"),
-        selected = "audience_score"
+        choices = c("Mintues", "Hours", "Sales", "Tracks"),
+        selected = "Sales"
       ),
       
       selectInput(
         inputId = "x", label = "X-axis:",
         choices = c("Year", "Minutes", "Genre"),
         selected = "critics_score"
+      ),
+      selectInput(
+        inputId = "genre", label = "Choose your preferred genre: ",
+        choices = c("Blues", "Classical", "Country", "EDM", "Hip Hop", "Jazz", "Pop", "R&B", "Rock", "World"),
+        selected = "Pop"
       )
     ),
     
     mainPanel(
       plotOutput(outputId = "scatterplot", brush = "plot_brush"),
       DT::dataTableOutput(outputId = "moviestable"),
+      
       br(),
       plotOutput(outputId = "salesBar"),
-      plotOutput(outputId = "salesLine")
+      plotOutput(outputId = "salesLine"),
+      br(),
+      DT::dataTableOutput(outputId = "genreTable"),
+      plotOutput(outputId = "genreBar")
     )
   )
 )
@@ -56,8 +66,28 @@ server <- function(input, output) {
   
   output$moviestable <- renderDataTable({
     brushedPoints(Albums, brush = input$plot_brush) %>%
-      select(Year, Artist, Album, Minutes, Sales)
+      select(Year, Album, Artist, Minutes, Sales)
   })
+  
+  
+  musicByGenre <- reactive({subset(Albums, (Genre == input$genre))})
+  
+  albumArtist <- unite(Albums, albumAndArtist, c(Album, Artist), sep = " - ", remove = FALSE)
+  musicByGenre10 <- reactive({head(arrange(subset(albumArtist, (Genre == input$genre)),desc(Sales)), n = 10)})
+  
+  
+  
+  
+  
+  
+  
+  output$genreTable <- renderDataTable(musicByGenre() %>%
+     select("Year", "Album", "Genre", "Artist", "Minutes", "Sales"),
+     options = list(
+       order = list(list(6, 'dsc')),
+       pageLength = 10
+     )
+     )
   
   salesByYear <- Albums %>%
     group_by(Year) %>%
@@ -71,6 +101,14 @@ server <- function(input, output) {
   output$salesLine <- renderPlot({
   ggplot(data = salesByYear) + 
     geom_line(mapping = aes(x = Year, y = TotalSales))
+  })
+  
+  output$genreBar <- renderPlot({
+    ggplot(musicByGenre10()) +
+      geom_bar(aes(x = Sales, y = reorder(albumAndArtist, +Sales)), stat="identity") +
+      scale_x_continuous(name="Total Album Sales", labels = scales::comma) +
+      ylab("Album Name") +
+      ggtitle(paste("If you like", input$genre, "may we suggest these albums:"))
   })
   
 }
